dbeea389d8341d7af7963175e4e91007c70f7771
ANG.YU <1029135136@qq.com>
2020-10-14 07:48:08 +0000

    完成用户注册功能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -13,6 +13,7 @@ class UsersController extends Controller
     {
         return view('users.create');
     }
+
     //显示用户个人信息的页面
     public function show(User $user)
     {
@@ -31,6 +32,14 @@ class UsersController extends Controller
             'email' => 'required|email|unique:users|max:255',
             'password' => 'required|confirmed|min:6'
         ]);
-        return;
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+        ]);
+        session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
+
+        return redirect()->route('users.show', $user);
     }
 }
