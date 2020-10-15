025efb5b3d732692a1910521c111fc0d0fc98704
ANG YU <1029135136@qq.com>
2020-10-15 17:16:34 +0800

    注册后自动登录

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UsersController extends Controller
 {
@@ -38,6 +39,8 @@ class UsersController extends Controller
             'email' => $request->email,
             'password' => bcrypt($request->password),
         ]);
+        //注册后自动登录
+        Auth::login($user);
         session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         //注册成功后，跳转到用户个人中心
         //return redirect()->route('users.show', $user->id);
